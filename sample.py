5570512afe76eeabce2a52838b60357bc177d347
ishwarya0103 <ishwaryaanandakrishnan@gmail.com>
2024-08-22 21:53:29 +0000

    updated with addition and multiplication

--- sample.py
+++ sample.py
@@ -1,1 +1,14 @@
-print("hello")
+# Ten-line Python program with an intentional error
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(x, y):
+    return x * y
+
+
+result1 = add_numbers(10, 5)
+result2 = multiply_numbers(4, 7)
+
+print("Sum:", result1)
+print("Product:" result2)  
+
