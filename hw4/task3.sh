926c0c33732b009c87a8d5e1721de4427a17ad36
ishwarya0103 <ishwaryaanandakrishnan@gmail.com>
2024-10-10 12:58:47 -0400

    Updated task3.sh

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-awk -F',' 'NR > 1 {
-    if ($3 == 2 && substr($13, 1, 1) == "S") {
-        print $0
-    }
-}' titanic.csv | \
-sed 's/,male/,M/g; s/,female/,F/g' > temp.txt
-
-awk -F',' '$7 != "" { total += $7; count++ } END { 
-    if (count > 0) {
-        print "Average Age:", total/count; 
-    } else { 
-        print "No valid passengers"; 
-    } 
-}' temp.txt
+awk -F',' 'NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") print $0 }' titanic.csv | sed 's/,male/,M/g; s/,female/,F/g' | tee temp1.txt | awk -F',' '$7 != "" { total += $7; count++ } END { if (count > 0) print "Average Age:", total/count; else print "No valid passengers" }'
