08f84edf41c96c7ae1ec0daf0955190074866e62
ishwarya0103 <ishwaryaanandakrishnan@gmail.com>
2024-10-10 13:20:55 -0400

    Updated task1.sh

--- hw4/task1.sh
+++ hw4/task1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+pgrep -f infinite.sh | xargs -r kill
+# pids=$(pgrep -f infinite.sh)
 
-pids=$(pgrep -f infinite.sh)
-
-if [ -n "$pids" ]; then
-    for pid in $pids; do
-        kill "$pid" && echo "Killed process with PID: $pid"
-    done
-else
-    echo "No infinite.sh process found."
-fi
+# if [ -n "$pids" ]; then
+#     for pid in $pids; do
+#         kill "$pid" && echo "Killed process with PID: $pid"
+#     done
+# else
+#     echo "No infinite.sh process found."
+# fi
