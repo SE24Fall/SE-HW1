504844d1df869e26faac4c2ed5678f717cab19fa
ishwarya0103 <ishwaryaanandakrishnan@gmail.com>
2024-10-11 01:41:56 +0000

    task2 updated

--- hw4/task2.sh
+++ hw4/task2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-grep -l "sample" dataset1/file* | 
-xargs -I {} sh -c 'count=$(grep -o "CSC510" {} | wc -l); if [ $count -ge 3 ]; then echo "{} has $count occurrences of CSC510"; fi' && grep -l "sample" dataset1/file* | 
-xargs -I {} sh -c 'count=$(grep -o "CSC510" {} | wc -l); if [ $count -ge 3 ]; then echo "{} $count"; fi' | 
-xargs -I {} sh -c 'file_name=$(echo {} | cut -d" " -f1); count=$(echo {} | cut -d" " -f2); size=$(stat -f %z $file_name); echo "$count $file_name $size"' | 
-sort -k1,1nr -k3,3n | sed 's/file_/filtered_/g'
+grep -l "sample" dataset1/file* | xargs grep -o "CSC510" | sort | uniq -c | grep -E '^[ ]3'|cut -d: -f1 | sed 's/^[ ]//;s/ *CSC510//' |
+           
+
+grep -l "sample" dataset1/file* | xargs -I {} sh -c 'count=$(grep -o "CSC510" {} | wc -l); size=$(stat -f %z {}); echo "$count {} $size" ' | awk '$1 >= 3 {print $0}' | sort -k1,1nr -k3,3n |
+
+
+grep -l "sample" dataset1/file* | xargs -I {} sh -c 'count=$(grep -o "CSC510" {} | wc -l); size=$(stat -f %z {}); echo "$count {} $size" ' | awk '$1 >= 3 {print $0}' | sort -k1,1nr -k3,3n |sed 's/file_/filtered_/g' 
+
